1172d5f03d502f061347c063a9a84831809abaa7
Abrar Malek <38218691+lazy-geek@users.noreply.github.com>
2023-02-14 21:59:15 +0530

    make the boardsize dynamic and move to class based approach

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
-
-//states
-// 0-> not played
-// 1-> selected by player 1
-// 2-> selected by player 2
-const boardTiles = [];
-
-
-
-let currentTurn = 1;
-
-const createMarker = (elemName) => {
-    const elem = document.createElement('div');
-    if(elemName == 'x'){
-        elem.classList.add('x');
-    }else{
-
-        elem.classList.add('o')
+class Board {
+    constructor(size) {
+        this.size = size;
+        this.boardTiles = []
+        // current turn
+        // 1 - player 1
+        // 2 - player 2
+        this.currentPlayer = 1;
+        this.setupBoardGrid();
+        this.init();
+        this.setupEventListeners();
     }
 
-    return elem;
-}
+    createTile() {
+        const elem = document.createElement('div');
+        elem.classList.add("board__tile");
+        return elem;
+    }
 
-const toggleTurn = ()=>{
-    if(currentTurn == 1){
-        currentTurn = 2;
-    }else{
-        currentTurn = 1;
+    setupBoardGrid() {
+        document.querySelector('.board').style.gridTemplateColumns = `repeat(${this.size},1fr)`;
+        document.querySelector('.board').style.gridTemplateRows = `repeat(${this.size},1fr)`;
+    }
+
+    toggleTurn() {
+        if (this.currentPlayer == 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
+    }
+
+    createMarker(elemName) {
+        const elem = document.createElement('div');
+        if (elemName == 'x') {
+            elem.classList.add('x');
+        } else {
+            elem.classList.add('o')
+        }
+
+        return elem;
+    }
+
+    init() {
+        const boardTiles = []
+        for (let i = 0; i < this.size; i++) {
+            const row = [];
+            for (let j = 0; j < this.size; j++) {
+
+                row.push({ "state": 0, "elem": this.createTile() });
+                document.querySelector('.board').appendChild(row[j].elem)
+            }
+
+            boardTiles.push(row);
+        }
+
+        this.boardTiles = boardTiles;
+    }
+
+    setupEventListeners() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                this.boardTiles[i][j].elem.addEventListener('click', () => {
+                    if (this.boardTiles[i][j].state != 0) return;
+                    this.boardTiles[i][j].state = this.currentPlayer;
+                    this.boardTiles[i][j].elem.appendChild(this.currentPlayer == 1 ? this.createMarker('x') : this.createMarker('o'));
+                    this.toggleTurn();
+                });
+            }
+
+        }
     }
 }
 
-let count = 1;
-for(let i=0; i<3;i++){
-    const row =[];
-    for(let j=0; j<3;j++){
-
-        row.push({"state":0 ,"elem": document.querySelector(`#tile_${count}`)});
-        count++;
-    }
-    boardTiles.push(row);
-}
-
-
-for(let i=0; i<boardTiles.length;i++){
-    for(let j=0; j<boardTiles[i].length;j++){
-        boardTiles[i][j].elem.addEventListener('click',function () {
-            if(boardTiles[i][j].state !== 0) return;
-            boardTiles[i][j].state = currentTurn;
-            boardTiles[i][j].elem.appendChild(currentTurn==1 ? createMarker('x') : createMarker('o'));
-            toggleTurn();
-        });
-    }
-}
+const temp = new Board(4);
