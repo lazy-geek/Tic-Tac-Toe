817976ca5e4422566463c49cc7861baf985210ad
Abrar Malek <38218691+lazy-geek@users.noreply.github.com>
2023-02-15 18:29:22 +0530

    Add game winner & game over logic - refactor making the move in different method - removeEventListener from board__tile after placing a piece on that board__tile

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ class Board {
         this.currentPlayer = 1;
         this.setupBoardGrid();
         this.init();
-        this.setupEventListeners();
+        this.maxMoves= size * size;
+        this.currentMoves = 0;
+       
     }
 
     createTile() {
@@ -55,21 +57,125 @@ class Board {
         }
 
         this.boardTiles = boardTiles;
+        this.setupEventListeners();
+
+    }
+    resetGame(){
+        this.currentMoves = 0;
+        this.currentPlayer = 1;
+
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+
+              
+                this.boardTiles[i][j].elem.remove();
+
+            }
+        }
+        this.init();
     }
 
+    checkGameOver(){
+        if(this.currentMoves === this.maxMoves){
+            console.log("gameOver - Draw");
+            setTimeout(() => {
+                window.alert("gameOver - Draw")
+                
+                this.resetGame();
+            }, 100);
+            return;
+        }
+    }
+    makeMove(i,j){
+        if (this.boardTiles[i][j].state != 0) return;
+                   
+        this.boardTiles[i][j].state = this.currentPlayer;
+        this.currentMoves++;
+        this.boardTiles[i][j].elem.appendChild(this.currentPlayer == 1 ? this.createMarker('x') : this.createMarker('o'));
+        const winner = this.checkWinner();
+        if(winner.winner == this.currentPlayer){
+            console.log("Player - " + winner.winner + " Won");
+            setTimeout(() => {
+                window.alert("Player - " + winner.winner + " Won")
+                
+                this.resetGame();
+            }, 100);
+            return;
+        }
+        this.checkGameOver();
+        this.boardTiles[i][j].elem.removeEventListener('click', () => {this.makeMove(i,j);});
+
+        this.toggleTurn();
+    }
     setupEventListeners() {
         for (let i = 0; i < this.size; i++) {
             for (let j = 0; j < this.size; j++) {
-                this.boardTiles[i][j].elem.addEventListener('click', () => {
-                    if (this.boardTiles[i][j].state != 0) return;
-                    this.boardTiles[i][j].state = this.currentPlayer;
-                    this.boardTiles[i][j].elem.appendChild(this.currentPlayer == 1 ? this.createMarker('x') : this.createMarker('o'));
-                    this.toggleTurn();
-                });
+                this.boardTiles[i][j].elem.addEventListener('click', () => {this.makeMove(i,j);});
             }
 
         }
     }
+
+    checkWinner() {
+        // check row by row
+        for (let i = 0; i < this.size; i++) {
+            let winnerFound = true;
+            for (let j = 1; j < this.size; j++) {
+                if (this.boardTiles[i][j - 1].state !== this.boardTiles[i][j].state || this.boardTiles[i][j].state === 0) {
+                    winnerFound = false;
+                    break;
+                }
+            }
+            if (winnerFound) {
+                return { "winner": this.boardTiles[i][0].state }
+            }
+
+        }
+
+        // check column by column
+        for (let i = 0; i < this.size; i++) {
+            let winnerFound = true;
+            for (let j = 1; j < this.size; j++) {
+                if (this.boardTiles[j - 1][i].state !== this.boardTiles[j][i].state || this.boardTiles[j][i].state === 0) {
+                    winnerFound = false;
+                    break;
+                }
+            }
+            if (winnerFound) {
+                return { "winner": this.boardTiles[i][0].state }
+            }
+
+        }
+
+        // check from upper left corner
+        let winnerFound = true;
+        for (let i = 1, j = 1; i < this.size && j < this.size; i++, j++) {
+            if (this.boardTiles[i - 1][j - 1].state !== this.boardTiles[i][j].state  || this.boardTiles[i][j].state === 0) {
+                winnerFound = false;
+                break;
+            }
+            
+
+        }
+        if (winnerFound) {
+            return { "winner": this.boardTiles[0][0].state }
+        }
+
+        // check from upper right corner
+        winnerFound = true;
+        for (let i = 0,j = this.size -1 ; i < this.size && j > 0; i++, j--) {
+            if (this.boardTiles[i][j].state !== this.boardTiles[i+1][j-1].state || this.boardTiles[i][j].state === 0) {
+                winnerFound = false;
+                break;
+            }
+        }
+        
+        if (winnerFound) {
+            return { "winner": this.boardTiles[this.size-1][0].state }
+        }
+
+        return {"winner": 0};
+    }
 }
 
-const temp = new Board(4);
+const temp = new Board(3);
